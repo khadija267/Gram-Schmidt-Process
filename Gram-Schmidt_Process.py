a3600c0631f516f4e72a977cde1a026fccb46128
khadija267 <37675645+khadija267@users.noreply.github.com>
2020-05-05 23:33:54 +0200

    Create Gram-Schmidt_Process.py

---
+++ Gram-Schmidt_Process.py
@@ -0,0 +1,52 @@
+# This function uses the Gram-schmidt process to calculate the dimension
+# spanned by a list of vectors.
+# Since each vector is normalised to one, or is zero,
+import numpy as np
+import numpy.linalg as la
+
+verySmallNumber = 1e-14 # That's 1×10⁻¹⁴ = 0.00000000000001
+def gsBasis(A) :
+    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
+    # Loop over all vectors, starting with zero, label them with i
+    for i in range(B.shape[1]) :
+        for j in range(i) :
+            B[:, i] = B[:,i]-B[:,i]@B[:,j]*B[:,j]
+        if la.norm(B[:, i]) > verySmallNumber :
+            B[:, i] = B[:, i] / la.norm(B[:, i])
+        else :
+            B[:, i] = np.zeros_like(B[:, i])
+    return B
+
+# the sum of all the norms will be the dimension.
+def dimensions(A) :
+    return np.sum(la.norm(gsBasis(A), axis=0))
+##########################TEST##################################
+V = np.array([[1,0,2,6],
+              [0,1,8,2],
+              [2,8,3,1],
+              [1,-6,2,3]], dtype=np.float_)
+gsBasis4(V)
+print(gsBasis(V))
+'''
+array([[ 0.40824829, -0.1814885 ,  0.04982278,  0.89325973],
+       [ 0.        ,  0.1088931 ,  0.99349591, -0.03328918],
+       [ 0.81649658,  0.50816781, -0.06462163, -0.26631346],
+       [ 0.40824829, -0.83484711,  0.07942048, -0.36063281]]
+'''
+print( dimensions(A))
+
+#3.0
+#what if our vector that is a linear combination of the others.
+C = np.array([[1,0,2],
+              [0,1,-3],
+              [1,0,2]], dtype=np.float_)
+print(gsBasis(C))
+'''
+array([[ 0.70710678,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ],
+       [ 0.70710678,  0.        ,  0.        ]])
+'''       
+
+print(dimensions(C))
+
+#2.0
